b29d931d02542c25741d4af966fc0ae1db825b9a
Mustafa Alper Sayan <alper.sayan@ozu.edu.tr>
2021-01-12 21:25:17 +0300

    Commit

---
+++ cs391_app/src/DeleteMovie.js
@@ -0,0 +1,12 @@
+
+
+function DeleteMovie() {
+    return (
+      <div>
+        <h1>Delete Movie Page</h1>
+      </div>
+    );
+  }
+  
+  export default DeleteMovie;
+  
---
+++ cs391_app/src/Movies.js
@@ -0,0 +1,120 @@
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import React, { Component } from 'react';
+
+class Movies extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Movies Page</h1>
+
+
+
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs lg="3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Salloon 1</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+
+
+            <Col xs lg="3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Salloon 2</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+
+
+
+            <Col xs lg="3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Salloon 3</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+          </Row>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <Row className="justify-content-md-center">
+            <Col xs lg="3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Salloon 4</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+
+
+            <Col xs lg="3">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Salloon 5</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+
+
+          </Row>
+        </Container>
+
+
+
+
+      </div>
+    );
+  }
+
+  
+  }
+  
+  export default Movies;
+  
